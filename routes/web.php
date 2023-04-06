8163b91b286273081fe5004f2ab6ada9872b2792
tejkys <tejkys@email.cz>
2023-04-06 18:13:07 +0200

    Sample http api calling

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [IndexController::class, 'show'])->name('index');
 Route::post('/auth', [AuthController::class, 'login'])->name('auth.login');
 Route::get('/auth', [AuthController::class, 'logout'])->name('auth.logout');
+
+Route::get('/test', [\App\Http\Controllers\TestingController::class, 'get'])->name('testing.get');
