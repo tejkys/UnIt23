e0195f68c4398bcc27fe37f870d6bf30b5fb3dfe
Ondřej Matějka <tejkys@email.cz>
2023-04-12 10:03:05 +0200

    Query params

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -10,6 +10,20 @@ class IndexController extends Controller
 {
     public function show(Request $request): View
     {
+        if(empty($request->query())
+            || !array_key_exists("objectId", $request->query())
+            || !array_key_exists("authSessionId", $request->query())
+            || !array_key_exists("companyUrl", $request->query())
+
+        ){
+            return view('index')->with(['message'=>'Vyžadují se parametry pro zpracování položky']);
+        }
+        session([
+                "objectId"=>explode(",",$request->query("objectId")),
+                "authSessionId"=> $request->query("authSessionId"),
+                "companyUrl"=> $request->query("companyUrl"),
+            ]
+        );
         return view('index');
     }
 }
