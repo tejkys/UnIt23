040737707ceacc722ff5bd3d39faad4fb419fed0
Tomdok <tomdokx@gmail.com>
2023-04-12 12:23:35 +0200

    #6 - table for results, small changes..

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,10 +3,11 @@
 @section('title', 'Vítejte')
 
 @section('content')
-    <div class="container-fluid bg-light w-100">
+    @foreach($invoices as $invoice)
+    <div class="container-fluid bg-light w-100 mb-3">
         <div class="border-bottom h-25 bg-white p-3">
             <div>
-                @foreach($invoices as $invoice)
+
                 <div class="d-block">
                     <div>
                         <span>Název firmy: {{ $invoice->nazFirmy }} </span>
@@ -19,7 +20,7 @@
                     </div>
                 </div>
                 <hr class="hr">
-                @endforeach
+
                 <div>
                     <div class="d-flex">
 
@@ -29,7 +30,7 @@
                     </div>
                     <div class="row pt-2">
                         <div class="col-4 d-flex flex-column text-center align-content-center">
-                            <div class="m-3">
+                            <div class="m-2">
                                 <select class="form-select form-select-lg mb-3">
                                     <option selected hidden value="">Vybrat sadu</option>
                                     {{--
@@ -42,9 +43,8 @@
                                     <option value="3">Three</option>
                                 </select>
                             </div>
-                            <div class="m-3 d-flex justify-content-center gap-3">
+                            <div class="m-0 d-flex justify-content-center">
                                 <button type="button" class="btn btn-dark p-2">Rozúčtovat</button>
-                                <button type="button" class="btn btn-dark p-2">Vytvořit sadu</button>
                             </div>
                         </div>
                         <div class="col-1"></div>
@@ -52,6 +52,7 @@
                             <table class="table border border-2">
                                 <thead>
                                 <tr>
+                                    <th scope="col">#</th>
                                     <th scope="col">Typ pravidla</th>
                                     <th scope="col">Hodnota</th>
                                     <th scope="col">Středisko</th>
@@ -59,9 +60,10 @@
                                 </thead>
                                 <tbody>
                                 <tr>
-                                    <td>Jan</td>
-                                    <td>Novák</td>
-                                    <td>25</td>
+                                    <td>1</td>
+                                    <td>Procenta</td>
+                                    <td>100</td>
+                                    <td>Středisko C</td>
                                 </tr>
                                 {{-- @foreach pravidla --}}
                                 <!-- Poslední řádek s tlačítkem + -->
@@ -77,8 +79,47 @@
                             </table>
                         </div>
                     </div>
+                    <div class="row d-flex justify-content-center">
+                        <table class="table table-hover w-75 border">
+                            <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Název</th>
+                                <th scope="col">Pravidlo</th>
+                                <th scope="col">Částka</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Název 1</td>
+                                <td>Pravidlo 1</td>
+                                <td>100 Kč</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Název 2</td>
+                                <td>Pravidlo 2</td>
+                                <td>200 Kč</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Název 3</td>
+                                <td>Pravidlo 3</td>
+                                <td>300 Kč</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">4</th>
+                                <td>Název 4</td>
+                                <td>Pravidlo 4</td>
+                                <td>400 Kč</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+    @endforeach
 @endsection
